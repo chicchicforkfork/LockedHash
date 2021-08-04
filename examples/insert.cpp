c3864564719f9792d92e4e0543a673ab09bd36fd
kinow83 <kinow83@gmail.com>
2021-08-04 12:54:15 +0900

    add insert test

---
+++ examples/insert.cpp
@@ -0,0 +1,37 @@
+#include "lockedhash.hpp"
+#include "person.hpp"
+#include <iostream>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+using namespace chkchk;
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  LockedHash<PersonKey, Person, PersonHash, PersonMakeKey> hash(100);
+
+  cout << "old lis\n";
+  hash(Person("P501", 501));
+  hash(Person("P502", 502));
+
+  hash.loop([](size_t bucket, Person &p) {
+    (void)bucket;
+    cout << p.to_string() << "\n";
+  });
+
+  cout << "\nafter lis\n";
+  Person p500 = Person("P500", 500);
+  p500.setData(100);
+  hash(PersonKey("P500", 500), //
+       p500,                   //
+       [](Person &p) { p.setData(100); });
+
+  hash.loop([](size_t bucket, Person &p) {
+    (void)bucket;
+    cout << p.to_string() << "\n";
+  });
+}
--- examples/person.hpp
+++ examples/person.hpp
@@ -8,15 +8,15 @@ using PersonKey = std::pair<std::string, int>;
 class Person {
 private:
   std::string _name;
-  int _empno;
-  int _cache;
+  int _empno = 0;
+  int _cache = 0;
+  int _data = 0;
 
 public:
   Person() {}
   Person(const PersonKey &k) : Person() {
     _name = k.first;
     _empno = k.second;
-    _cache = 0;
   }
   Person(const std::string &name, int empno) : Person() {
     _name = name;
@@ -27,10 +27,13 @@ public:
   int empno() const { return _empno; }
   void setEmpno(int empno) { _empno = empno; }
   void hit() { _cache++; }
+  void setData(int data) { _data = data; }
+  int data() { return _data; }
 
   std::string to_string() {
     return "name: " + _name + ", empno: " + std::to_string(_empno) +
-           "cache: " + std::to_string(_cache);
+           ", cache: " + std::to_string(_cache) +
+           ", data: " + std::to_string(_data);
   }
 };
 
